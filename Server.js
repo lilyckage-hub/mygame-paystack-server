f9ae344bb6aac5eaa82712b526f2cb02dcd7005d
lilyckage-hub <lilyckage@gmail.com>
2026-07-14 01:30:43 +0000

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,1 +1,1 @@
-Const app equals express. Const port equals process .env dot port or 3000. Then app . use express dot JSON. App dot get slash send hello from Paystack backend. App listen on port then log server running on port
+Const app equals express. Const port equals process .env .port or 3000. Then app . use express.JSON. App. get /send hello from Paystack backend. App listen on port then log server running on port
